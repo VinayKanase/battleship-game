315a9063dc83440a7c30146f6a2ffabd46044ad3
Vinay S Kanse <vinaykanse9451@gmail.com>
2021-08-18 22:18:26 +0530

    Onworking ShipsPlacementAI Completed On page User input for ships

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import "../styles/style.css";
 import GameBoard from "./GameBoard";
 import Ship from "./Ship";
+import PlaceShipsToGameBoard from './ShipsPlacementAI';
 // fade, fadebottom, fadeleft, faderight, fadetop, glow
 
 function fancytext(mainClass, styletype = "fade", timer = 50) {
@@ -42,8 +43,11 @@ function addBlocksToGameBoard() {
   let grid = document.querySelectorAll('.gameBoard');
   grid.forEach((gr)=>{
     for(let i = 0; i < 10; i++){
+      let row = document.createElement('div');
+      row.classList.add('row');
+      gr.append(row);
       for(let j = 0; j < 10; j++){
-        gr.innerHTML += `<div data-coordinates="${i}${j}" class='gridItem'></div>`;
+        row.innerHTML += `<div data-coordinates="${i}${j}" class='gridItem'></div>`;
       }
     }
   });
@@ -53,32 +57,140 @@ addBlocksToGameBoard();
 const domManager = (function(){
   const setShips = document.getElementById('setShips');
   const gameBoard =  document.getElementById('setPlayerGameBoard');
-  let shipCount = 0;
+  let shipCount = 5;
+  let isOkToPlaceShip = false;
+  let rotate = false;
+  let playerCanStartGame = false;
   function init(){
     setShips.classList.remove('display-none');
-    console.log(gameBoard);
+    activeBoard();
+  }
+  function activeBoard(){
     gameBoard.addEventListener("mouseover",(e)=>{
-      if(shipCount === 0){
-        // e.target.classList.add('placedShip');
-        // console.log(e.target.nextSibling);
-        // e.target.nextSibling.classList.add('placedShip');
-        // e.target.nextSibling.nextSibling.classList.add('placedShip');
-        //  TODO : Find way to add ship at place of hover and in order of decreasing size of ship
+      if(!e.target.classList.contains('gridItem')) return;
+      let x = +e.target.dataset.coordinates.slice(0,1);
+      let y = +e.target.dataset.coordinates.slice(1);
+      clearSetBoard();
+      for(let i = 0; i < shipCount;i++){
+        let pos;
+        if(rotate){
+          if(!gameBoard.children[x+i]) {
+            isOkToPlaceShip = false;
+            return clearSetBoard();  
+          }
+          pos = gameBoard.children[x+i].children[y];
+        }
+        else if(!rotate){
+          pos = gameBoard.children[x].children[y+i];
+        }
+        if(!pos || pos.classList.contains('shipDrop')) {
+          isOkToPlaceShip = false;
+          return clearSetBoard();
+        }
+        pos.classList.add('placedShip');
+        isOkToPlaceShip = true;
       }
+    });
+  }
+  gameBoard.addEventListener("click",(e)=>{
+    if(!e.target.classList.contains('gridItem') || playerCanStartGame) return;
+    if(isOkToPlaceShip){
+      let coordinatesArray = [];
+      let x = +e.target.dataset.coordinates.slice(0,1);
+      let y = +e.target.dataset.coordinates.slice(1);
+      for(let i = 0; i < shipCount;i++){
+        let pos;
+        if(rotate){
+          if(!gameBoard.children[x+i]) {
+            isOkToPlaceShip = false;
+            return clearSetBoard();  
+          }
+          pos = gameBoard.children[x+i].children[y];
+        }
+        else if(!rotate){
+          pos = gameBoard.children[x].children[y+i];
+        }
+        if(!pos) {
+          isOkToPlaceShip = false;
+          return clearSetBoard();
+        }
+        document.getElementById(shipCount).style.opacity = '0.5';
+        pos.classList.add('shipDrop');
+        coordinatesArray.push(`${rotate ? x+i : x}${rotate ? y : y+i}`);
+      }
+      shipCount--;
+      let count = Game.createShip(coordinatesArray);
+      if(count == 5) playerCanStartGame = true;
+    }
+  });
+  document.getElementById('clear').addEventListener("click",()=>{
+      clearSetBoard();
+      document.querySelectorAll('.shipDrop').forEach(ship=>{
+        ship.classList.remove('shipDrop');
+      })
+      shipCount = 5;
+      isOkToPlaceShip = false;
+  });
+  document.getElementById('rotate').addEventListener("click",()=>rotate=!rotate);
+  document.getElementById('beginGame').addEventListener("click",()=>{
+    if(playerCanStartGame){
+      document.getElementById('setShips').classList.add('display-none');
+      document.getElementById('mainPage').classList.remove('display-none');
+      beginGame();
+    }
+  });
+  function clearSetBoard(){
+    let placedShips = document.querySelectorAll('.placedShip');
+    placedShips.forEach(ship=>{
+      ship.classList.remove('placedShip');
+    })
+  }
+  function beginGame(){
+    let ships = Game.totalShips().map((ship)=>ship.getshipPositions());
+    let playerBoard = document.getElementById('playerBoard');
+    ships.forEach(ship=>{
+      ship.forEach(pos=>{
+        let element = playerBoard.querySelector(`[data-coordinates="${pos.coordinates}"`);
+        element.classList.add('shipDrop');
+      }); 
+    });
+
+    let oships = Game.returnComputerShips().map((ship)=>ship.getshipPositions());
+    let opponentBoard = document.getElementById('opponentBoard');
+    console.log(oships);
+    oships.forEach(ship=>{
+      ship.forEach(pos=>{
+        let element = opponentBoard.querySelector(`[data-coordinates="${pos.coordinates}"`);
+        element.classList.add('shipDrop');
+      }); 
     });
   }
   return {init};
 })();
 const Game = (function(){
   let gameBoard;
+  let opponentGameBoard;
   function init(){
     domManager.init();
     gameBoard = new GameBoard();
+    opponentGameBoard = new GameBoard();
+    PlaceShipsToGameBoard().forEach((ship)=>opponentGameBoard.addShip(new Ship(ship)));
   }
-  function createShip(){
-    // let ship = new Ship()
+  function createShip(shipCoordinates){
+    let ship = new Ship(shipCoordinates);
+    gameBoard.addShip(ship);
+    if(gameBoard.ships.length > 5){
+      gameBoard.ships = gameBoard.ships.slice(5,6);
+    }
+    return gameBoard.ships.length;
   }
-  return {init}
+  function returnComputerShips(){
+    return opponentGameBoard.ships;
+  }
+  function totalShips(){
+    return gameBoard.ships;
+  }
+  return {init, createShip, totalShips, returnComputerShips}
 })();
 
 document.getElementById('start').addEventListener("click",()=>{
